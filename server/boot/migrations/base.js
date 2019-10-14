0030dbd132859eb6d8d52de4f1d4e0708a1a4005
Asdruval De Leon <asdruval@Asdruvals-MacBook-Pro-2.local>
2019-10-13 21:05:51 -0400

    initial api commit

---
+++ server/boot/migrations/base.js
@@ -0,0 +1,33 @@
+// ensure correct server.js file for the migration script
+const path = require('path')
+
+const server = require(path.resolve(__dirname, '../../server.js'))
+const { mysqlDb } = server.datasources
+
+const models = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Client']
+
+module.exports = function migrateBaseModels (app, next) {
+  console.log('\n---\nBeginning Migrations...')
+
+  // are models in sync with DB?
+  mysqlDb.isActual(models, (err, actual) => {
+    if (err) throw err
+
+    const syncStatus = actual ? 'in sync' : 'out of sync'
+
+    console.log('\nModels are ${syncStatus}')
+
+    if (actual) return next()
+
+    console.log('Migrating Models...')
+
+    // create the tables for us and/or migrates them with new properties
+    return mysqlDb.autoupdate(models, (err) => {
+      if (err) throw err
+
+      console.log('Models migration successful!\n')
+
+      next()
+    })
+  })
+}
