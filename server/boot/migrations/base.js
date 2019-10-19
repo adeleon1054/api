8f5b066781d25fca89aec5846ec3ce20e85e418a
Asdruval De Leon <asdruval@Asdruvals-MacBook-Pro-2.local>
2019-10-19 15:10:13 -0400

    protected widgets features

--- server/boot/migrations/base.js
+++ server/boot/migrations/base.js
@@ -1,33 +1,41 @@
 // ensure correct server.js file for the migration script
-const path = require('path')
+const path = require("path");
 
-const server = require(path.resolve(__dirname, '../../server.js'))
-const { mysqlDb } = server.datasources
+const server = require(path.resolve(__dirname, "../../server.js"));
+const { mysqlDb } = server.datasources;
 
-const models = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'Client']
+const models = [
+  "User",
+  "AccessToken",
+  "ACL",
+  "RoleMapping",
+  "Role",
+  "Client",
+  "Widget"
+];
 
-module.exports = function migrateBaseModels (app, next) {
-  console.log('\n---\nBeginning Migrations...')
+module.exports = function migrateBaseModels(app, next) {
+  console.log("\n---\nBeginning Migrations...");
 
   // are models in sync with DB?
   mysqlDb.isActual(models, (err, actual) => {
-    if (err) throw err
+    if (err) throw err;
 
-    const syncStatus = actual ? 'in sync' : 'out of sync'
+    const syncStatus = actual ? "in sync" : "out of sync";
 
-    console.log('\nModels are ${syncStatus}')
+    console.log("\nModels are ${syncStatus}");
 
-    if (actual) return next()
+    if (actual) return next();
 
-    console.log('Migrating Models...')
+    console.log("Migrating Models...");
 
     // create the tables for us and/or migrates them with new properties
-    return mysqlDb.autoupdate(models, (err) => {
-      if (err) throw err
+    return mysqlDb.autoupdate(models, err => {
+      if (err) throw err;
 
-      console.log('Models migration successful!\n')
+      console.log("Models migration successful!\n");
 
-      next()
-    })
-  })
-}
+      next();
+    });
+  });
+};
